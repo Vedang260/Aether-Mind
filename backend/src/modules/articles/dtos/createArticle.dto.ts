45faefbe3b566914094d9c7ac35e5188f92fe4f4
Vedang <vedang.wappnet@gmail.com>
2025-04-03 15:24:49 +0530

    create Article dto is created

---
+++ backend/src/modules/articles/dtos/createArticle.dto.ts
@@ -0,0 +1,26 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class CreateArticleDto{
+    @IsNotEmpty()
+    @IsString()
+    author_id: string;
+
+    @IsNotEmpty()
+    @IsString()
+    category_id: string;
+
+    @IsString()
+    @IsNotEmpty()
+    title: string;
+
+    @IsString()
+    @IsNotEmpty()
+    description: string;
+    
+    @IsNotEmpty()
+    @IsString()
+    content: string;
+
+    @IsNotEmpty()
+    tags: string[];
+}
