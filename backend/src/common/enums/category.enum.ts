96f07200f88c9bcb249d48174c453492a930702f
Vedang <vedang.wappnet@gmail.com>
2025-04-03 13:34:03 +0530

    Category enum is created

---
+++ backend/src/common/enums/category.enum.ts
@@ -0,0 +1,11 @@
+export enum Category {
+    TECHNOLOGY = 'Technology',
+    SCIENCE = 'Science',
+    BUSINESS_FINANCE = 'Business & Finance',
+    HEALTH_MEDICINE = 'Health & Medicine',
+    EDUCATION = 'Education & Learning',
+    LIFESTYLE_PRODUCTIVITY = 'LifeStyle & Productivity',
+    ENTERTAINMENT = 'Entertainment & Media',
+    SPORTS = 'Sports',
+    HISTORY_CULTURE = 'History & Culture'
+}
