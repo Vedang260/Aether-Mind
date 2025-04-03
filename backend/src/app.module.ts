25f16b8ac29b58b278c43b131d34e6ba342bbc62
Vedang <vedang.wappnet@gmail.com>
2025-04-03 18:25:25 +0530

    bull module is added

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,10 +4,18 @@ import { UsersModule } from './modules/users/modules/users.module';
 import { AuthModule } from './modules/auth/module/auth.module';
 import { typeOrmConfig } from './config/database.config';
 import { AuthMiddleware } from './modules/auth/middlewares/auth.middleware';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { bullConfig } from './config/queue.config';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(typeOrmConfig),
+    BullModule.forRootAsync({                 // New BullMQ setup
+      useFactory: bullConfig,
+      inject: [ConfigService],
+    }),
     AuthModule,
     UsersModule,
   ],
