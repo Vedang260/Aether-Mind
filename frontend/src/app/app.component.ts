37b2c890f5a6f0d42de273d9822d1f106d5b94e6
Vedang <vedang.wappnet@gmail.com>
2025-04-07 13:10:36 +0530

    Conditional showing of navbar is implemented

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, RouterOutlet],
+  imports: [NavbarComponent, RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Aether Mind';
+  showNavbar: boolean = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.updateNavbarVisibility(event.urlAfterRedirects);
+      }
+    });
+  }
+
+  updateNavbarVisibility(url: string) {
+    // Define routes where you want to show the navbar
+    const showOnRoutes = ['/dashboard', '/article'];
+
+    // Check if the current URL starts with any of these routes
+    this.showNavbar = showOnRoutes.some(route => url.startsWith(route));
+  }
 }
