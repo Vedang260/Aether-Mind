545681de3e84f085a505e0dbf1ffa0c09133b2f0
Vedang <vedang.wappnet@gmail.com>
2025-04-10 01:11:05 +0530

    analytcis is implemented

--- frontend/src/app/core/services/analytics.service.ts
+++ frontend/src/app/core/services/analytics.service.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs';
 export class AnalyticsService {
     token = localStorage.getItem('auth_token');
     headers = new HttpHeaders({ 'Authorization': `Bearer ${this.token}` });
-    private apiUrl = 'http://localhost:8000/api/analytics/categorywise-analytics'; // replace with your API endpoint
+    private apiUrl = 'http://localhost:8000/api/analytics/categories-analytics'; // replace with your API endpoint
 
   constructor(private http: HttpClient) {}
 
